0821942a6d8ad390ebaf0775ba8aa4ab6b8a0100
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2024-08-03 15:37:22 +0530

    user-model-added

---
+++ backend/models/user-model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+// Define the schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zipCode: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+},{
+    timestamps: true,
+});
+
+// Create the model
+const User = mongoose.model('User', userSchema);
+
+// Export the model
+module.exports = {User};
