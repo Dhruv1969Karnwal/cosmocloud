d2d0c2104412b0a3a30b583b1ea61d24229d27ce
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2024-08-03 18:11:08 +0530

    vite-config-updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://127.0.0.1:9090',
+      '/api': 'https://cosmocloud-backend.vercel.app/',
     },
   },
 })
